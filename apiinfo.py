4eac0be612913f5fbafa3c474ae94991ca22df25
NumanMaroof085-1 <numanmaroof085@gmail.com>
2025-09-03 11:31:55 +0500

    only for testnet environment

--- apiinfo.py
+++ apiinfo.py
@@ -4,12 +4,6 @@ import os
 # Replace these with your actual keys from the testnet page
 api_key = os.environ.get('BINANCE_TESTNET_API_KEY')
 api_secret = os.environ.get('BINANCE_TESTNET_SECRET_KEY')
-# api_key = "D7jUfM1q8pzN8403xK6NiRKdJKifJXWUB1SeodY1R4H8LcEIABUtJxUwNvJ1100I"
-# api_secret = "odgBguZ8VUIkF6ahADOVpfOgPWBtwS3psjGeIkmvu7pwtnPA0yR46Puo2ztCCI52"
-# api_key = "JfnT3XkD4EEiZRv52mOp9EuU46FsGd74wgOgs3BEueB43htM4fijw7CuF50Wd8vr"
-# api_secret = "oWQ8xvddWipUyBlLJvmHExmOAzlz7Y1GqORtqPvxS1IJJztS33zxVezg46mJFhMX"
-# api_key = "sdQoABZMq6R2Q1uYWcYYnbWtpCTANxnMqppZdJWJXRMkTn9mKvRXEmJU9aRygh51"
-# api_secret = "LPpE8VGtxCXsRQtViPhsNO5fJLrU8xk3kaxa2mFGpL9lNzOCl5x28zrOaK5IRAxL"
 
 print(os.environ.get('BINANCE_TESTNET_API_KEY'))
 print(os.environ.get('BINANCE_TESTNET_SECRET_KEY'))
@@ -51,6 +45,8 @@ account_info = client.get_account()
 
 # Cancel an order using its ID
 # cancel_order = client.cancel_order(symbol='BTCUSDT', orderId=17788655)
+# print(cancel_order)
+
 def cancel_all_orders(symbol):
     """Cancel all open orders for a given symbol."""
     try:
@@ -71,14 +67,12 @@ def cancel_all_orders(symbol):
         print(f"✗ Error cancelling orders: {e}")
         return None
 # print(cancel_all_orders('BTCUSDT'))
-# print(cancel_order)
 
 # order = client.order_market_sell(symbol= 'BTCUSDT',quantity = 0.9199)
 # print(f"Market sell order placed successfully: {order}")
 
 # Filter for only BTC and USDT
 target_assets = ['BTC', 'USDT']
-
 for balance in account_info['balances']:
     if balance['asset'] in target_assets:
         free_balance = float(balance['free'])
@@ -116,4 +110,5 @@ for balance in account_info['balances']:
 # )
 open_orders = client.get_open_orders(symbol='BTCUSDT')
 print(open_orders)
+
 
